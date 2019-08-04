1f35240008076fc9e9829f7e46bd0114015342f6
ShivamPrajapati-dev <shivamprajapati.git@gmail.com>
2019-08-04 11:00:10 +0530

    added backdrop

--- app/src/main/java/com/room_rental/rent_it_room/MainActivity.java
+++ app/src/main/java/com/room_rental/rent_it_room/MainActivity.java
@@ -3,12 +3,27 @@ package com.room_rental.rent_it_room;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearLayout=findViewById(R.id.front_layer);
+        BottomSheetBehavior sheetBehavior=BottomSheetBehavior.from(linearLayout);
+
+        sheetBehavior.setFitToContents(false);
+        sheetBehavior.setHideable(false);
+        sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+
+
+
     }
 }
